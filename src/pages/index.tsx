f264bcdb83d668ca871f10726dc71ea71aae8c34
Simlex <gettosimlex@gmail.com>
2023-08-03 06:37:44 +0100

    Created homepage

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.scss'
+import HeroSection from '@/components/Homepage/HeroSection'
+import FeaturedEvents from '@/components/Homepage/FeaturedEvents'
+import Services from '@/components/Homepage/Services'
+import CreateEvent from '@/components/Homepage/CreateEvent'
+
+export default function Home() {
+  return (
+    <div className={styles.homepage}>
+        <HeroSection />
+        <FeaturedEvents />
+        <Services />
+        <CreateEvent />
+    </div>
+  )
+}
