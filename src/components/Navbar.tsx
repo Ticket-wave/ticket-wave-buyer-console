277c637b50a06503d220d0576dd7a602adfd223d
Simlex <gettosimlex@gmail.com>
2023-08-12 21:17:47 +0100

    Updated navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,17 @@ import { FunctionComponent, ReactElement, useState, useRef } from 'react';
 import styles from '../styles/Navbar.module.scss';
 import Image from 'next/image';
 import images from '../../public/images';
-import { CaretDownIcon, MoonIcon, SunIcon, UserIcon } from './SVGs/SVGicons';
+import { CaretDownIcon, HamburgerMenuIcon, MoonIcon, SunIcon, UserIcon } from './SVGs/SVGicons';
 import useOuterClick from '@/hooks/useOuterClick';
+import useResponsive from '@/hooks/useResponsiveness copy';
 
 interface NavbarProps {
 
 }
 
 const Navbar: FunctionComponent<NavbarProps> = (): ReactElement => {
+
+    const onMobile = useResponsive();
 
     const [navbarDropdownIsVisible, setNavbarDropdownIsVisible] = useState(false);
     const [mobileNavbarIsVisible, setMobileNavbarIsVisible] = useState(false);
@@ -20,38 +23,52 @@ const Navbar: FunctionComponent<NavbarProps> = (): ReactElement => {
     useOuterClick(navbarDropdownRef, setNavbarDropdownIsVisible);
 
     return (
-        <section className={styles.navbarContainer}>
-            <div className={styles.navbarContainer__lhs}>
-                <div className={styles.logo}>
-                    <Image src={images.logoWhite} alt='Logo' />
-                </div>
-                <p>Ticketwave</p>
-            </div>
-            <div className={styles.navbarContainer__rhs}>
-                <ul className={styles.navLinks}>
-                    <li>Home</li>
-                    <li>Events</li>
-                    <li>Support</li>
-                </ul>
-                <div className={styles.accountSection} ref={navbarDropdownRef}>
-                    <div className={styles.profileCircle}>
-                        <UserIcon />
-                    </div>
-                    <h3>Account</h3>
-                    <span className={styles.dropdownIcon} onClick={() => setNavbarDropdownIsVisible(!navbarDropdownIsVisible)}>
-                        <CaretDownIcon />
-                    </span>
-                    {navbarDropdownIsVisible &&
-                        <div className={styles.dropdownContainer}>
-                            <span>My account</span>
-                            <span>Log out</span>
-                        </div>}
-                </div>
-                <span className={styles.themeController}>
-                    {isLightTheme ? <MoonIcon /> : <SunIcon />}
-                </span>
-            </div>
-        </section>
+        <>
+            {
+                onMobile ?
+                    <section className={styles.mobileNavbarContainer}>
+                        <div className={styles.logo}>
+                            <Image src={images.logoWhite} alt='Logo' />
+                        </div>
+                        <div className={styles.buttons}>
+                            <span><SunIcon /></span>
+                            <span><HamburgerMenuIcon /></span>
+                        </div>
+                    </section> :
+                    <section className={styles.navbarContainer}>
+                        <div className={styles.navbarContainer__lhs}>
+                            <div className={styles.logo}>
+                                <Image src={images.logoWhite} alt='Logo' />
+                            </div>
+                            <p>Ticketwave</p>
+                        </div>
+                        <div className={styles.navbarContainer__rhs}>
+                            <ul className={styles.navLinks}>
+                                <li>Home</li>
+                                <li>Events</li>
+                                <li>Support</li>
+                            </ul>
+                            <div className={styles.accountSection} ref={navbarDropdownRef}>
+                                <div className={styles.profileCircle}>
+                                    <UserIcon />
+                                </div>
+                                <h3>Account</h3>
+                                <span className={styles.dropdownIcon} onClick={() => setNavbarDropdownIsVisible(!navbarDropdownIsVisible)}>
+                                    <CaretDownIcon />
+                                </span>
+                                {navbarDropdownIsVisible &&
+                                    <div className={styles.dropdownContainer}>
+                                        <span>My account</span>
+                                        <span>Log out</span>
+                                    </div>}
+                            </div>
+                            <span className={styles.themeController}>
+                                {isLightTheme ? <MoonIcon /> : <SunIcon />}
+                            </span>
+                        </div>
+                    </section>
+            }
+        </>
     );
 }
 
