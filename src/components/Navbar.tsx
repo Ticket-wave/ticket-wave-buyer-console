0f345fa06aa5f2fac2012e86aa666dd3ebcc20cd
Simlex <gettosimlex@gmail.com>
2023-08-13 17:43:12 +0100

    Updated navbar component for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,19 @@ import { FunctionComponent, ReactElement, useState, useRef } from 'react';
 import styles from '../styles/Navbar.module.scss';
 import Image from 'next/image';
 import images from '../../public/images';
-import { CaretDownIcon, HamburgerMenuIcon, MoonIcon, SunIcon, UserIcon } from './SVGs/SVGicons';
+import { CaretDownIcon, CloseMenuIcon, HamburgerMenuIcon, MoonIcon, SunIcon, UserIcon } from './SVGs/SVGicons';
 import useOuterClick from '@/hooks/useOuterClick';
 import useResponsive from '@/hooks/useResponsiveness copy';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface NavbarProps {
 
 }
 
 const Navbar: FunctionComponent<NavbarProps> = (): ReactElement => {
+
+    const router = useRouter();
 
     const onMobile = useResponsive();
 
@@ -22,26 +26,57 @@ const Navbar: FunctionComponent<NavbarProps> = (): ReactElement => {
 
     useOuterClick(navbarDropdownRef, setNavbarDropdownIsVisible);
 
+    const [navbarIsVisible, setNavbarIsVisible] = useState(false);
+
+    const navbarRef = useRef<HTMLDivElement>(null);
+
+    // useRemoveHtmlElementFromDOM(navbarRef, navbarIsVisible, 600, "flex");
+
     return (
         <>
             {
                 onMobile ?
                     <section className={styles.mobileNavbarContainer}>
-                        <div className={styles.logo}>
-                            <Image src={images.logoWhite} alt='Logo' />
-                        </div>
-                        <div className={styles.buttons}>
-                            <span><SunIcon /></span>
-                            <span><HamburgerMenuIcon /></span>
-                        </div>
-                    </section> :
-                    <section className={styles.navbarContainer}>
-                        <div className={styles.navbarContainer__lhs}>
+                        <Link href='/'>
                             <div className={styles.logo}>
                                 <Image src={images.logoWhite} alt='Logo' />
                             </div>
-                            <p>Ticketwave</p>
+                        </Link>
+                        <div className={styles.buttons}>
+                            <span><SunIcon /></span>
+                            <span onClick={() => setNavbarIsVisible(true)}><HamburgerMenuIcon /></span>
                         </div>
+                        {navbarRef &&
+                            <div className={navbarIsVisible ? styles.navbarOverlayContainer : styles.navbarOverlayContainerClose} ref={navbarRef}>
+                                <span className={styles.overlay} onClick={() => setNavbarIsVisible(false)}></span>
+                                <div className={styles.navbarOverlay}>
+                                    <span className={styles.closeIcon} onClick={() => setNavbarIsVisible(false)}><CloseMenuIcon /></span>
+                                    <div className={styles.navLinks}>
+                                        <Link href='/' onClick={() => setNavbarIsVisible(false)}>
+                                            <span className={router.pathname == '/' ? styles.active : ''}>Home</span>
+                                        </Link>
+                                        <Link href='/events' onClick={() => setNavbarIsVisible(false)}>
+                                            <span className={router.pathname == '/events' ? styles.active : ''}>Events</span>
+                                        </Link>
+                                        <Link href='/support' onClick={() => setNavbarIsVisible(false)}>
+                                            <span className={router.pathname == '/support' ? styles.active : ''}>Support</span>
+                                        </Link>
+                                        <Link href='/account' onClick={() => setNavbarIsVisible(false)}>
+                                            <span className={router.pathname == '/account' ? styles.active : ''}>Account</span>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>}
+                    </section> :
+                    <section className={styles.navbarContainer}>
+                        <Link href='/'>
+                            <div className={styles.navbarContainer__lhs}>
+                                <div className={styles.logo}>
+                                    <Image src={images.logoWhite} alt='Logo' />
+                                </div>
+                                <p>Ticketwave</p>
+                            </div>
+                        </Link>
                         <div className={styles.navbarContainer__rhs}>
                             <ul className={styles.navLinks}>
                                 <li>Home</li>
