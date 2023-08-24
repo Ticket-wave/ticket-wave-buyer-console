8005c8c93a8aa7478e60650439694fbe510f0878
Simlex <gettosimlex@gmail.com>
2023-08-24 03:26:42 +0100

    Updated tooltip component

--- src/components/custom/Tooltip.tsx
+++ src/components/custom/Tooltip.tsx
@@ -3,12 +3,13 @@ import { ReactElement, FunctionComponent } from "react";
 interface TooltipProps {
     children: JSX.Element
     tooltipText: string
+    position?: 'left' | 'bottom' | 'top' | 'right'
 }
 
-const Tooltip: FunctionComponent<TooltipProps> = ({ children, tooltipText }): ReactElement => {
+const Tooltip: FunctionComponent<TooltipProps> = ({ children, tooltipText, position }): ReactElement => {
     return (
         <div className='tooltipWrapper'>
-            <span className='tooltip'>{tooltipText}</span>
+            <span className={`tooltip ${position ?? 'left'}`}>{tooltipText}</span>
             {children}
         </div>
     );
