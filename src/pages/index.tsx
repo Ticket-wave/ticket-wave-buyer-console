d8ab0b14566e739715bf669c3950e777d8914627
Simlex <simlexafol@gmail.com>
2023-11-01 09:22:24 +0100

    New metadata implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,49 @@ import HeroSection from '@/components/Homepage/HeroSection'
 import FeaturedEvents from '@/components/Homepage/FeaturedEvents'
 import Services from '@/components/Homepage/Services'
 import CreateEvent from '@/components/Homepage/CreateEvent'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
     return (
         <>
-            <Head>
+            <NextSeo
+                title="Your Title"
+                description="This is a demo description"
+                canonical="https://www.example.com"
+                openGraph={{
+                    url: 'https://www.example.com',
+                    title: 'Open Graph Title',
+                    description: 'Open Graph Description',
+                    images: [
+                        {
+                            url: '../../public/images/event_flyer.jpg',
+                            width: 800,
+                            height: 600,
+                            alt: 'Og Image Alt',
+                            type: 'image/jpeg',
+                        },
+                        {
+                            url: '../../public/images/event_flyer.jpg',
+                            width: 900,
+                            height: 800,
+                            alt: 'Og Image Alt Second',
+                            type: 'image/jpeg',
+                        },
+                        { url: '../../public/images/event_flyer.jpg' },
+                        { url: '../../public/images/event_flyer.jpg' },
+                    ],
+                    site_name: 'YourSiteName',
+                }}
+                twitter={{
+                    handle: '@handle',
+                    site: '@site',
+                    cardType: 'summary_large_image',
+                }}
+            />
+            {/* <Head>
                 <title>Ticket wave web application</title>
                 <meta name="description" content="Ticket wave web application" />
-                {/* Add other common metadata here */}
+                
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -31,7 +66,7 @@ export default function Home() {
                 <meta name="twitter:image" content="../../public/images/event_flyer.jpg" />
 
                 <meta name="image" content="../../public/images/event_flyer.jpg" />
-            </Head>
+            </Head> */}
             <div className={styles.homepage}>
                 <HeroSection />
                 <FeaturedEvents />
