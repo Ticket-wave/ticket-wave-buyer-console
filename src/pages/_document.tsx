e930ad039cc310b514edaec5642e33b1b68fd763
Simlex <simlexafol@gmail.com>
2023-11-01 09:58:15 +0100

    Updated metadat image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,22 +10,22 @@ export default function Document() {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-                <meta name="description" content="Application description" />
+                <meta name="description" content="Application description for ticket wave" />
 
                 <meta property="og:url" content="https://ticketwave.netlify.app" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Ticket wave web application" />
-                <meta property="og:description" content="Application description" />
-                <meta property="og:image" content="../../public/images/event_flyer.jpg" />
+                <meta property="og:description" content="Application description for ticket wave - OG" />
+                <meta property="og:image" content="https://ticketwave.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fevent_flyer.63358925.jpg&w=1920&q=75" />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="twitter:domain" content="ticketwave.netlify.app" />
                 <meta property="twitter:url" content="https://ticketwave.netlify.app" />
                 <meta name="twitter:title" content="Ticket wave web application" />
                 <meta name="twitter:description" content="Application description" />
-                <meta name="twitter:image" content="../../public/images/event_flyer.jpg" />
+                <meta name="twitter:image" content="https://ticketwave.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fevent_flyer.63358925.jpg&w=1920&q=75" />
 
-                <meta name="image" content="../../public/images/event_flyer.jpg" />
+                <meta name="image" content="https://ticketwave.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fevent_flyer.63358925.jpg&w=1920&q=75" />
             </Head>
             <body>
                 <Main />
