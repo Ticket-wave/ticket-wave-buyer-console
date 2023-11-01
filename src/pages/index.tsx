69284ce7150d28536c8fc38866ece061dcec1030
Simlex <simlexafol@gmail.com>
2023-11-01 09:14:32 +0100

    Updated metadata info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,25 @@ export default function Home() {
                 <title>Ticket wave web application</title>
                 <meta name="description" content="Ticket wave web application" />
                 {/* Add other common metadata here */}
-                <meta property="og:title" content="Social Title for Cool Page" />
-                <meta property="og:description" content="And a social description for our cool page" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                <meta name="description" content="Application description" />
+
+                <meta property="og:url" content="https://simlex.netlify.app" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Portfolio Website" />
+                <meta property="og:description" content="Application description" />
                 <meta property="og:image" content="../../public/images/event_flyer.jpg" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="simlex.netlify.app" />
+                <meta property="twitter:url" content="https://simlex.netlify.app" />
+                <meta name="twitter:title" content="Portfolio Website" />
+                <meta name="twitter:description" content="Application description" />
+                <meta name="twitter:image" content="../../public/images/event_flyer.jpg" />
+
+                <meta name="image" content="../../public/images/event_flyer.jpg" />
             </Head>
             <div className={styles.homepage}>
                 <HeroSection />
