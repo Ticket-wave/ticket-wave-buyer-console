f3bbc77bd6b739bd3b3f87591ea1c5aceca86271
Simlex <simlexafol@gmail.com>
2023-11-01 09:00:13 +0100

    Updated metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,26 +12,9 @@ export default function Home() {
                 <title>Ticket wave web application</title>
                 <meta name="description" content="Ticket wave web application" />
                 {/* Add other common metadata here */}
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-
-                <meta property="og:image" content="../../public/images/event_flyer.jpg" />
-                <meta property="og:image:type" content="image/jpeg" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-
-                {/* Facebook Meta Tags */}
-                <meta property="og:url" content="https://ticketwave.netlify.app/" />
-                <meta property="og:type" content="image/jpeg" />
-                <meta property="og:title" content="Your Site Title" />
-                <meta property="og:description" content="Common description for your site" />
-                <meta property="og:image" content="../../public/images/event_flyer.jpg" />
-
-                {/* Twitter Meta Tags */}
-                <meta name="twitter:image" content="../../public/images/event_flyer.jpg" />
-                <meta name="twitter:image:type" content="image/jpeg" />
-                <meta name="twitter:image:width" content="1200" />
-                <meta name="twitter:image:height" content="630" />
+                <meta property="og:title" content="Social Title for Cool Page" />
+                <meta property="og:description" content="And a social description for our cool page" />
+                <meta property="og:image" content="https://example.com/images/cool-page.jpg" />
             </Head>
             <div className={styles.homepage}>
                 <HeroSection />
