0b392eaf975f78a5e3f6ec276fa29a9b59b0401a
Simlex <simlexafol@gmail.com>
2023-12-14 23:28:29 +0100

    Updated functionality

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,9 @@ import { ToastContext } from '@/extensions/toast';
 import { ToastMessageType } from './models/ToastMessageType';
 import ToastCard from './Card/ToastCard';
 import Provider from './Provider';
+import Sidebar from './shared/Sidebar';
+import Topbar from './shared/Topbar';
+import { useRouter } from 'next/router';
 
 export const metadata: Metadata = {
     title: 'Ticket wave web application',
@@ -18,7 +21,13 @@ interface LayoutProps {
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }): ReactElement => {
 
+    const { pathname } = useRouter();
+
     const toastContext = useContext(ToastContext);
+    const isAppPage = pathname.includes('/app');
+    const isEventsPage = pathname == '/app/events'; 
+    const isViewEventPage = pathname.startsWith('/app/event') && !pathname.includes('/create');
+    
 
     return (
         <>
@@ -29,9 +38,26 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }): ReactElement => {
                     description={toastContext?.toastOptions?.description ?? ''}
                     messageType={toastContext?.toastOptions?.type ?? ToastMessageType.Info}
                     timeout={toastContext?.toastOptions?.timeout ?? 0.01} />
-                <Navbar />
-                {children}
-                <Footer />
+                {
+                    !isAppPage && <>
+                        <Navbar />
+                        {children}
+                        <Footer />
+                    </>
+                }
+                {
+                    isAppPage && <>
+                        <div className="appLayout">
+                            <Topbar />
+                            <div className="appLayout__body">
+                                <Sidebar />
+                                <div className="innerBody" style={(isEventsPage || isViewEventPage) ? {padding: 0} : {}}>
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                }
             </Provider>
         </>
     )
